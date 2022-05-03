e6a8f9c09d65f992b6304dc37a6628beadaac88a
Lara <75126217+little-evil-genius@users.noreply.github.com>
2022-05-03 12:42:29 +0200

    Create residences.lang.php

---
+++ inc/languages/deutsch_du/residences.lang.php
@@ -0,0 +1,72 @@
+<?php
+
+// ÜBERSICHT
+$l['residences'] = 'Wohnübersicht';
+
+// WOHNORT HINZUFÜGEN
+$l['residences_home_add'] = 'Wohnort hinzufügen';
+$l['residences_home_add_street'] = 'Straßen';
+$l['residences_home_add_street_desc'] = 'Straße auswählen';
+$l['residences_home_add_type'] = 'Wohnart';
+$l['residences_home_add_type_desc'] = 'Bei Apartment bitte - Apartment Nummer Buchstabe (2B)';
+$l['residences_home_add_number'] = 'Hausnummer';
+$l['residences_home_add_number_desc'] = 'Nur die Zahl';
+$l['residences_home_add_personcount'] = 'Bewohneranzahl';
+$l['residences_home_add_personcount_desc'] = 'Maximal Anzahl der Bewohner. Nur die Zahl!';
+$l['residences_home_add_send'] = 'Wohnort hinzufügen';
+
+// STRASSE HINZUFÜGEN
+$l['residences_streets_add'] = 'Straße hinzufügen';
+$l['residences_streets_add_name'] = 'Straßennamen';
+$l['residences_streets_add_rate'] = 'Bewertung';
+$l['residences_streets_add_desc'] = 'Beschreibung';
+$l['residences_streets_add_send'] = 'Straße hinzufügen';
+
+// WOHNORT BEARBEITEN
+$l['residences_home_edit'] = 'Wohnort bearbeiten';
+$l['residences_home_edit_send'] = 'Wohnort editieren';
+
+// STRASSE BEARBEITEN
+$l['residences_streets_edit'] = 'Straße bearbeiten';
+$l['residences_streets_edit_send'] = 'Straße editieren';
+
+// MODCP
+$l['residences_modcp_nav_home'] = 'Wohnorte freischalten';
+$l['residences_modcp_nav_street'] = 'Straßen freischalten';
+
+$l['residences_modcp_home'] = 'Neue Wohnorte verwalten';
+$l['residences_modcp_home_bit_accept'] = 'Wohnort annehmen';
+$l['residences_modcp_home_bit_delete'] = 'Wohnort ablehnen';
+
+$l['residences_modcp_streets'] = 'Neue Straßen verwalten';
+$l['residences_modcp_streets_bit_accept'] = 'Straße annehmen';
+$l['residences_modcp_streets_bit_delete'] = 'Straße ablehnen';
+
+// WEITERLEITUNGSSEITEN
+$l['residences_join'] = 'Du bist erfolgreich in diesen Wohnort eingezogen und wirst nun zurückgeleitet!';
+$l['residences_leave'] = 'Du bist erfolgreich aus diesem Wohnort ausgezogen und wirst nun zurückgeleitet!';
+$l['residences_delete_home'] = 'Du hast diesen Wohnort und all seine Bewohner erfolgreich gelöscht und wirst nun zurückgeleitet!';
+$l['residences_delete_street'] = 'Du hast die Straße und all Wohnorte innerhalb und deren Bewohner erfolgreich gelöscht und wirst nun zurückgeleitet!';
+$l['residences_add_street'] = 'Du hast erfolgreich eine neue Straße hinzugefügt und wirst nun zurückgeleitet!';
+$l['residences_add_home'] = 'Du hast erfolgreich einen neuen Wohnort hinzugefügt und wirst nun zurückgeleitet!';
+$l['residences_edit_street'] = 'Du hast die Sraße erfolgreich berarbeitet und wirst nun zurückgeleitet!';
+$l['residences_edit_home'] = 'Du hast die Sraße erfolgreich berarbeitet und wirst nun zurückgeleitet!';
+$l['residences_modcp_delete_street'] = 'Du hast erfolgreich die neue Straße abgelehnt und wirst nun zurückgeleitet!';
+$l['residences_modcp_accept_street'] = 'Du hast erfolgreich die neue Straße angenommen und wirst nun zurückgeleitet!';
+$l['residences_modcp_delete_home'] = 'Du hast erfolgreich den neuen Wohnort abgelehnt und wirst nun zurückgeleitet!';
+$l['residences_modcp_accept_home'] = 'Du hast erfolgreich den neuen Wohnort angenommen und wirst nun zurückgeleitet!';
+
+// MyALERTS STUFF
+$l['residences_street_delete'] = '{1} hat deine Straße <strong>"{4}"</strong> abgelehnt!';
+$l['myalerts_setting_residences_street_delete'] = "Benachrichtigung bei Ablehnung vom Team bei hinzugefügten Straßen?";
+
+$l['residences_street_accept'] = '{1} hat deine Straße <strong>"{4}"</strong> angenommen!';
+$l['myalerts_setting_residences_street_accept'] = "Benachrichtigung bei Annahme vom Team bei hinzugefügten Straßen?";
+
+$l['residences_home_delete'] = '{1} hat deinen Wohnort <strong>"{4} {5}"</strong> abgelehnt!';
+$l['myalerts_setting_residences_home_delete'] = "Benachrichtigung bei Ablehnung vom Team bei hinzugefügten Wohnorten?";
+
+$l['residences_home_accept'] = '{1} hat deinen Wohnort <strong>"{4} {5}"</strong> angenommen!';
+$l['myalerts_setting_residences_home_accept'] = "Benachrichtigung bei Annahme vom Team bei hinzugefügten Wohnorten?";
+
+?>
